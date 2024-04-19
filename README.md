85bcc0af21670edcbd044c33414bb2cc20eda7c6
LeeHyunJong <vvlddiz@gmail.com>
2024-04-19 10:45:15 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# 240419_java_workspace1" 
---
+++ java_Work/src/ex_240419/Food.java
@@ -0,0 +1,10 @@
+package ex_240419;
+
+public class Food {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("오늘 점심 메뉴 추천");
+	}
+
+}
